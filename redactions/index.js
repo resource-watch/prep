302ebeced22d1f20b47efa6bc2b4636486aef353
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-08-02 11:11:44 +0200

    Remove unnecessary code

--- redactions/index.js
+++ redactions/index.js
@@ -1,8 +1,4 @@
 export { default as common } from './common';
-export { default as explore } from './explore';
-export { default as exploreDetail } from './exploreDetail';
-export { default as pulse } from './pulse';
-export { default as partnerDetail } from './partnerDetail';
 export { default as tooltip } from './tooltip';
 export { default as staticPages } from './static_pages';
 export { default as modal } from './modal';
--- routes.js
+++ routes.js
@@ -26,27 +26,3 @@ routes.add('admin_widgets', '/admin/widgets/:tab?/:subtab?', 'admin/Widgets');
 
 // USERS
 routes.add('admin_users', '/admin/users/:tab?/:subtab?', 'admin/users');
-
-// ========================= APP ROUTES =====================
-routes.add('home', '/', 'app/Home');
-
-// routes.add('about', '/about', 'app/About');
-// routes.add('about_partners', '/about_partners', 'app/Partners');
-// routes.add('partner', '/about/partners/:id', 'app/PartnerDetail');
-
-// routes.add('data', '/data', 'app/Explore'); // TODO: create the data page
-// routes.add('explore', '/explore', 'app/Explore');
-// routes.add('explore_detail', '/explore/:id', 'app/ExploreDetail');
-
-// routes.add('pulse', '/pulse', 'app/Pulse');
-
-// routes.add('dashboards', '/dashboards/:slug?', 'app/Dashboards');
-
-// routes.add('insights', '/insights', 'app/Home'); // TODO: create the insights page
-
-// ----- GET INVOLVED -----
-// routes.add('get_involved', '/get-involved', 'app/GetInvolved');
-// routes.add('get_involved_detail', '/get-involved/:id', 'app/GetInvolvedDetail');
-
-// ------ MY RW ------------
-routes.add('myprep', '/myprep/:tab?/:subtab?', 'app/MyRW');
